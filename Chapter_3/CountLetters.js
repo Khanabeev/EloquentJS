09e677e20d58df5ad9e002bb475420d51f4a1317
Anton Khanabeev <khanabeev@gmail.com>
2020-04-28 05:31:04 +0300

    Exercise Chapter 3

--- Chapter_3/CountLetters.js
+++ Chapter_3/CountLetters.js
@@ -1,15 +1,15 @@
 "use strict"
 
-function countBs(str) {
-    return countChar(str, "B")
+function countBs( str ) {
+    return countChar( str, "B" )
 }
 
-function countChar(str, char) {
+function countChar( str, char ) {
     let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === char) count++
+    for ( let i = 0; i < str.length; i++ ) {
+        if ( str[i] === char ) count++
     }
     return count
 }
 
-console.log(countBs("My Brother Big B"))
+console.log( countBs( "My Brother Big B" ) )
